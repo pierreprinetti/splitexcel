79e39114f05ae1acf5a0b9abd4a35722777a0765
Pierre Prinetti <qrawl@users.noreply.github.com>
2014-06-05 14:32:49 +0200

    create crude installer

---
+++ splitexcel-installer.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+mkdir -p C:\Progr\splitexcel
+copy splitexcel.vbs C:\Progr\splitexcel\
+
+reg add HKEY_CURRENT_USER\Software\Classes\Excel.Sheet.12\shell\split /ve /d "Split every sheet in a separate Excel file"
+reg add HKEY_CURRENT_USER\Software\Classes\Excel.Sheet.12\shell\split\command /ve /d "C:\Windows\System32\wscript.exe \"C:\\Progr\\splitexcel\\splitexcel.vbs\" \"%%1\""
+
+reg add HKEY_CURRENT_USER\Software\Classes\Excel.Sheet.8\shell\split /ve /d "Split every sheet in a separate Excel file"
+reg add HKEY_CURRENT_USER\Software\Classes\Excel.Sheet.8\shell\split\command /ve /d "C:\Windows\System32\wscript.exe \"C:\\Progr\\splitexcel\\splitexcel.vbs\" \"%%1\""
